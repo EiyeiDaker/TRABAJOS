94f20e17fdeb653cd2c7f5f3a8680c58d67652d3
admin <admin@DESKTOP-1K2DAMO>
2024-09-04 11:08:25 -0500

    Primerr repositorio en GitHub - java

--- src/TRABAJOS/EntradaySalidaEjercicio3.java
+++ src/TRABAJOS/EntradaySalidaEjercicio3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//mostrar dos numeros ingresadoas..
+//mostrar dos numeros ingresadoas...
 package TRABAJOS;
 import java.util.Scanner;
 
