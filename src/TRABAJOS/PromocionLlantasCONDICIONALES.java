4f909aa766f20f5f4a5a4347ba5d1260b9d42bca
admin <admin@DESKTOP-1K2DAMO>
2024-09-04 10:55:33 -0500

    Primerr repositorio en GitHub - java

--- src/TRABAJOS/PromocionLlantasCONDICIONALES.java
+++ src/TRABAJOS/PromocionLlantasCONDICIONALES.java
@@ -5,7 +5,7 @@
 package TRABAJOS;
 import java.util.Scanner;
 
-// En una serviteca see ha establecido una promoción de las llantas marca “Pinchadas”, que consiste  en lo siguiente: 
+// En una serviteca se ha establecido una promoción de las llantas marca “Pinchadas”, que consiste  en lo siguiente: 
 //• Si se compran menos de cinco llantas el precio es de U$100 cada una, de U$75 si se  compran de cinco a 10 y de U$50 si se compran más de 10. 
 //• Obtener la cantidad de dinero que una persona tiene que pagarrr por cada una de las llantas  que compra y la que tiene que pagar por el total de la compra. 
 
