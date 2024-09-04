78de5955f783a323fd536da74c75c9904d5a7736
admin <admin@DESKTOP-1K2DAMO>
2024-09-04 10:50:06 -0500

    Primer repositorio en GitHub - java

--- src/TRABAJOS/EntradaSalidaEjercicio1.java
+++ src/TRABAJOS/EntradaSalidaEjercicio1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//ingresar nombre y que muestre nombre
+//ingresar nombre y que muestre nombre.
 package TRABAJOS;
 import java.util.Scanner;
 /**
