7183aa8d61efa45c813ac34dd2fceb549516f465
admin <admin@DESKTOP-1K2DAMO>
2024-10-30 12:36:23 -0500

    TRABAJOS HERENCIA - 1/15

---
+++ src/Alfombra___/AlfombraCuadrada.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Alfombra___;
+
+/**
+ *
+ * @author admin
+ */
+public class AlfombraCuadrada extends Alfombra {
+    private int lado;
+
+    // Constructor
+    public AlfombraCuadrada(String color, int precioMetro, int lado) {
+        super(color, precioMetro);
+        this.lado = lado;
+    }
+
+    // Implementación de métodos de la interfaz
+    @Override
+    public double calcularSuperficie() {
+        return lado * lado; // Superficie de un cuadrado
+    }
+
+    @Override
+    public double calcularPrecio() {
+        return calcularSuperficie() * getPrecioMetro(); // Precio total
+    }
+
+    // Getters y Setters
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+}
+
---
+++ src/Alfombra___/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Alfombra___;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Crear objetos de las clases hijas
+        AlfombraRedonda alfombraRedonda = new AlfombraRedonda("Rojo", 20, 5);
+        AlfombraCuadrada alfombraCuadrada = new AlfombraCuadrada("Azul", 25, 4);
+
+        // Calcular y mostrar la superficie y el precio de la alfombra redonda
+        System.out.println("Alfombra Redonda:");
+        System.out.println("Color: " + alfombraRedonda.getColor());
+        System.out.println("Superficie: " + alfombraRedonda.calcularSuperficie() + " m²");
+        System.out.println("Precio: " + alfombraRedonda.calcularPrecio() + " €\n");
+
+        // Calcular y mostrar la superficie y el precio de la alfombra cuadrada
+        System.out.println("Alfombra Cuadrada:");
+        System.out.println("Color: " + alfombraCuadrada.getColor());
+        System.out.println("Superficie: " + alfombraCuadrada.calcularSuperficie() + " m²");
+        System.out.println("Precio: " + alfombraCuadrada.calcularPrecio() + " €");
+    }
+}
